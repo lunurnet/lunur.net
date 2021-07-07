df4b17bf00ff877017df73e03c7ad0aef8326e0b
Adrian <adrianalexander17@gmail.com>
2021-07-06 23:13:34 -0400

    Sus

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Sus from "../views/Sus.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/sus",
+    name: "Sus",
+    component: Sus,
   },
 ];
 
