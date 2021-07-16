3e96060b5b60671348273b0ce3fb44d507b44bef
Adrian <adrianalexander17@gmail.com>
2021-07-16 10:49:18 -0400

    cleaned up script

--- src/scripts.js
+++ src/scripts.js
@@ -1,31 +1,3 @@
-// State
-let navbarOpen = false;
-
-/**
- * function to toggle navbar open and closed based on global state (defaults closed)
- */
-function toggleNavbar() {
-  const openBtn = document.getElementById("navbarBtnOpen");
-  const closeBtn = document.getElementById("navbarBtnClose");
-  const mobileMenu = document.getElementById("mobileMenu");
-
-  navbarOpen = !navbarOpen;
-
-  if (navbarOpen) {
-    openBtn.classList.remove("block");
-    openBtn.classList.add("hidden");
-    closeBtn.classList.remove("hidden");
-    closeBtn.classList.add("block");
-    mobileMenu.classList.remove("hidden");
-  } else {
-    closeBtn.classList.remove("block");
-    closeBtn.classList.add("hidden");
-    openBtn.classList.remove("hidden");
-    openBtn.classList.add("block");
-    mobileMenu.classList.add("hidden");
-  }
-}
-
 /**
  * function to hide/show astronaut
  */
@@ -59,19 +31,10 @@ function scrollUp() {
   element.scrollIntoView({ behavior: "smooth" });
 }
 
-/**
- * Add toggle navbar function to mobile hamburger menu
- */
-// document
-//   .getElementById("navbarBtnToggleMobile")
-//   .addEventListener("click", function () {
-//     toggleNavbar();
-//   });
-
 // export for tests
 if (typeof exports !== "undefined") {
   module.exports = {
-    toggleNavbar,
+    // toggleNavbar,
     toggleAstronaut,
     scrollUp,
     scrollToAbout,
